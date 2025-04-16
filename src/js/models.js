17dc3cfc7a2f896914d6a9beee99497f430c7970
omar <omarfarah2002@hotmail.com>
2025-04-16 16:17:49 +0200

    Added OOP structure with Medicine class and subclasses for prescription and OTC types

---
+++ src/js/models.js
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from "uuid";
+
+export class Medicine {
+    constructor(productName, manufacturer, expirationDate, quantity) {
+        this.productId = uuidv4(); // Unik ID
+        this.productName = productName;
+        this.manufacturer = manufacturer;
+        this.expirationDate = new Date(expirationDate).toISOString(); // ISO 8601-format
+        this.quantity = quantity;
+    }
+}
+
+export class PrescriptionMedicine extends Medicine {
+    constructor(productName, manufacturer, expirationDate, quantity, doctorName) {
+        super(productName, manufacturer, expirationDate, quantity);
+        this.requiresPrescription = true;
+        this.doctorName = doctorName;
+    }
+}
+
+export class OverTheCounterMedicine extends Medicine {
+    constructor(productName, manufacturer, expirationDate, quantity) {
+        super(productName, manufacturer, expirationDate, quantity);
+        this.requiresPrescription = false;
+    }
+}
