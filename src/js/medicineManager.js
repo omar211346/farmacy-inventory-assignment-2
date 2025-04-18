79ca3331477156e8cd607a425166c06b341f25ac
omar <omarfarah2002@hotmail.com>
2025-04-18 14:13:33 +0200

    fixed bugs and mispelling after analyzing

--- src/js/medicineManager.js
+++ src/js/medicineManager.js
@@ -1,38 +1,32 @@
 export class MedicineManager {
-    
-    static getMedicine() { 
-        return JSON.parse(localStorage.getitem("medcines")) || []; 
+    static getMedicines() {
+      return JSON.parse(localStorage.getItem("medicines")) || [];
     }
-
-    static saveMedicine(data) { 
-        localStorage.setItem("medicines", JSON.stringfy(data)); 
+  
+    static saveMedicines(data) {
+      localStorage.setItem("medicines", JSON.stringify(data));
     }
-
+  
     static addMedicine(newMedicine) {
-        let meds = MedicineManager.getMedicines; 
-        meds.push(medicine); 
-        MedicineManager.saveMedicines(meds); 
+      let meds = MedicineManager.getMedicines();
+      meds.push(newMedicine);
+      MedicineManager.saveMedicines(meds);
     }
-
+  
     static deleteMedicine(id) {
-        let data = this.getMedicines(); 
-        data = data.filter((m) => m.id !== id); 
-        this.saveMedicines(data); 
+      let data = this.getMedicines();
+      data = data.filter((m) => m.id !== id);
+      this.saveMedicines(data);
     }
-
-    static updateMedicine(id, data) {
-        const list = MedicineManager.getMedicines(); 
-        let index = -1;
-        for (let i = 0; i < list.length; i++) {
-            if (list[i].productId = id) { 
-                index = i;
-            }
-        }
-
-        if (index != -1) { 
-            list[index] = data 
-        }
-
+  
+    static updateMedicine(id, updatedData) {
+      const list = MedicineManager.getMedicines();
+      let index = list.findIndex((m) => m.id === id);
+  
+      if (index !== -1) {
+        list[index] = updatedData;
         this.saveMedicines(list);
+      }
     }
-}
+  }
+  
